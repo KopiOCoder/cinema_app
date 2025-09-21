4c93719bec8dffb7e88c7fcf3b22879d6ab8f374
Harsimran <hsj030609@gmail.com>
2025-09-21 20:18:48 +0800

    Switching from web to app based and adding in json db for food and drinks

--- Fud.py
+++ Fud.py
@@ -1,2 +1,18 @@
-print("skibidi toilet")
-print("hi")
+import tkinter as tk
+from tkinter import font
+import json
+
+#Open Json DB for food and drinks 
+
+def OpenJsonDB():
+    try: 
+        with open("FoodDrinks.json", "r") as file:
+            return json.load(file)
+    except json.JSONDecodeError:
+        print("Error file gone wrong")
+    except FileNotFoundError:
+        print("Cant find file")
+
+root = tk.Tk()
+root.mainloop()
+
