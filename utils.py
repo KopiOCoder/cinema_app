c55a9761df02e022e047fba895716eca8d5d41cc
Alex Chiam <chiamjuinhoong@gmail.com>
2025-09-21 14:18:04 +0800

    feat: updated feature list

--- utils.py
+++ utils.py
@@ -8,5 +8,19 @@ def create_features(genres):
     if "Action" in genres: features[1] = 1.0
     if "Drama" in genres: features[2] = 1.0
     if "Animation" in genres: features[3] = 1.0
+    if "Musical" in genres: features[4] = 1.0
+    if "Children" in genres: features[5] = 1.0
+    if "Mystery" in genres: features[6] = 1.0
+    if "Horror" in genres: features[7] = 1.0
+    if "Fantasy" in genres: features[8] = 1.0
+    if "Western" in genres: features[9] = 1.0
+    if "Sci-Fi" in genres: features[10] = 1.0
+    if "Crime" in genres: features[11] = 1.0
+    if "Documentary" in genres: features[12] = 1.0
+    if "IMAX" in genres: features[13] = 1.0
+    if "Adventure" in genres: features[14] = 1.0
+    if "Thriller" in genres: features[15] = 1.0
+    if "Film-Noir" in genres: features[16] = 1.0
+    if "War" in genres: features[17] = 1.0
 
     return features
